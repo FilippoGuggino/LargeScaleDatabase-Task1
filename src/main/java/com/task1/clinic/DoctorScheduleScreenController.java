f8472da9424911e2d7ed9ea6f5cc79135f01f66d
zaccaria-essaid <zaccaria-essaid@hotmail.it>
2019-10-25 22:06:14 +0200

    aggiunte tutte le finestre tranne quelle di employee. da risolvere il resizing dinamico delle finestre.

---
+++ src/main/java/com/task1/clinic/DoctorScheduleScreenController.java
@@ -0,0 +1,28 @@
+package com.task1.clinic;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class DoctorScheduleScreenController {
+
+    @FXML
+    private void switchToInitial() throws IOException {
+            App.setRoot("initialScreen");
+    }
+    @FXML
+    private void updateSchedule() throws IOException {
+        DatePicker selectedDate=(DatePicker)App.scene.lookup("#selectedDate");
+        Label errorLabel=(Label) App.scene.lookup("#errorLabel");
+        if(selectedDate.getValue()==null){
+            errorLabel.setText("Select a date!");
+            errorLabel.setVisible(true);
+        }
+        else{
+            System.out.println(selectedDate.getValue().toString());
+            //TODO updateTable
+        }
+    }
+}
---
+++ src/main/java/com/task1/clinic/PatientScheduleScreenController.java
@@ -0,0 +1,29 @@
+package com.task1.clinic;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class PatientScheduleScreenController {
+
+    @FXML
+    private void switchToInitial() throws IOException {
+        App.setRoot("initialScreen");
+    }
+    @FXML
+    private void addMedical() throws IOException {
+        TextField firstNameInput=(TextField) App.scene.lookup("#firstnameInput");
+        TextField lastNameInput=(TextField) App.scene.lookup("#lastnameInput");
+        DatePicker selectedDate=(DatePicker)App.scene.lookup("#selectedDate");
+        Label errorLabel=(Label) App.scene.lookup("#errorLabel");
+        if(selectedDate.getValue()==null|| firstNameInput.getText().equals("")||lastNameInput.getText().equals("")){
+            errorLabel.setText("All fields must be filled!");
+            errorLabel.setVisible(true);
+        }
+        else{
+            //TODO addmedical
+        }
+    }
+}
